143b147b7002c6d41fc662b98dfe70cb87997d75
kraskoo <kraskoo87@gmail.com>
2016-11-01 16:51:23 +0200

    Initial commit

---
+++ In Developed/C#/FallingRocks/ColorFactory.cs
@@ -0,0 +1,30 @@
+﻿namespace FallingRocks
+{
+    using System;
+
+    public class ColorFactory
+    {
+        public static ConsoleColor GetColor(char rockType)
+        {
+            switch (rockType)
+            {
+                case '@':
+                    return ConsoleColor.DarkGreen;
+                case '#':
+                    return ConsoleColor.Magenta;
+                case '$':
+                    return ConsoleColor.White;
+                case '%':
+                    return ConsoleColor.Red;
+                case '^':
+                    return ConsoleColor.Green;
+                case '&':
+                    return ConsoleColor.DarkCyan;
+                case '*':
+                    return ConsoleColor.Yellow;
+                default:
+                    throw new ArgumentException("Unknown type.");
+            }
+        }
+    }
+}
